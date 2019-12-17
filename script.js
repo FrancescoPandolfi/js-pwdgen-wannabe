075e5059d6edc7e6c86915312a247ae1797efcf7
Francesco Pandolfi <francesco.pan@gmail.com>
2019-12-17 17:52:38 +0100

    End

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Chiedo i dati e li salvo nelle variabili
 var name = prompt('Come ti chiami?');
 var surname = prompt('Qual è il tuo cognome?');
-var eta = prompt('E la tua età?');
+var eta = parseInt(prompt('E la tua età?'));
 
 // Stampo i dati salvati nell'html
 document.getElementById('name').innerHTML = name;
@@ -10,6 +10,5 @@ document.getElementById('surname').innerHTML = surname;
 document.getElementById('eta').innerHTML = eta;
 
 // Genero la password e la stampo nell'html
-
 var pw = name + surname + eta + 19;
 document.getElementById('pw').innerHTML = pw;
